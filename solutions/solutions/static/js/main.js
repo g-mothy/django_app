48618c3942471744bb5a10ab90666b2518a27f15
g-mothy <gobillamothy85@gmail.com>
2019-02-05 19:56:55 +0530

    register and login forms

--- solutions/solutions/static/js/main.js
+++ solutions/solutions/static/js/main.js
@@ -1,2 +1,20 @@
 const date = new Date();
 document.querySelector('.year').innerHTML = date.getFullYear();
+
+
+var countDownDate = new Date("Feb 15, 2019 09:30:00").getTime();
+var x = setInterval(function() {
+var now = new Date().getTime();
+var distance = countDownDate - now;
+var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+document.getElementById("demo").innerHTML = days + "d " + hours + "h "
++ minutes + "m " + seconds + "s ";
+if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+}
+}, 1000);
+
